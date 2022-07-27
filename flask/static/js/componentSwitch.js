54411a8ebf2cedd094d9e29f969236b170417012
Hriday Keswani <hridaykeswani@gmail.com>
2022-07-27 09:51:14 +0530

    update ui

--- flask/static/js/componentSwitch.js
+++ flask/static/js/componentSwitch.js
@@ -112,6 +112,11 @@ function switchFourToFive() {
     form5.style.display = 'inline';
 }
 
+function switchFiveToFour(){
+    document.getElementById('form5').style.display = 'none';
+    document.getElementById('form4').style.display = 'inline';
+}
+
 function switchFiveToSix() {
     let form5 = document.getElementById('form5');
     let form6 = document.getElementById('form6');
@@ -140,6 +145,11 @@ function switchFiveToSix() {
 
     form5.style.display = 'none';
     form6.style.display = 'inline';
+}
+
+function swithcSixToFive(){
+    document.getElementById('form6').style.display = 'none';
+    document.getElementById('form5').style.display = 'inline';
 }
 
 function switchSixToSeven() {
@@ -180,6 +190,18 @@ function switchSixToSeven() {
     form6.style.display = 'none';
     form7.style.display = 'inline';
 }
+
+function switchSevenToSix(){
+    document.getElementById('form7').style.display = 'none';
+    document.getElementById('form6').style.display = 'inline';
+}
+
+function switchEighttoSeven(){
+    document.getElementById('form7').style.display = 'none';
+    document.getElementById('form6').style.display = 'inline';
+}
+
+
 function switchSeventoEight() {
 
     let form7 = document.getElementById('form7');
