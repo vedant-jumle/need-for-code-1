66d197cf7826bd2a6c11b1186d770cdb5ed32464
maxDeCoder <vedjum@gmail.com>
2022-07-27 05:32:41 +0530

    change componentSwitch.ejs

--- flask/hack.py
+++ flask/hack.py
@@ -11,7 +11,7 @@ df = pd.read_json('./content/jobless.json')
 df1= pd.read_csv('./dataset/career_pred.csv')
 
 print("loading model")
-model = ML_module("./models/main.h5")
+model = ML_module.CareerAdvisor("./models/main.h5")
 print("Model loaded")
 
 model_output = None
@@ -34,36 +34,36 @@ def fetch_and_send_details():
     global model_output
 
     res={
-    "Acedamic percentage in Operating Systems": request.form["Acedamic OS"],
-    "percentage in Algorithms": request.form["percentage Algo"],
-    "Percentage in Programming Concepts": request.form["percentage PC"],
-    "Percentage in Software Engineering": request.form["percentage SE"],
-    "Percentage in Computer Networks": request.form["percentage CN"],
-    "Percentage in Electronics Subjects": request.form["percentage ES"],
-    "Percentage in Computer Architecture": request.form["percentage CA"],
-    "Percentage in Mathematics": request.form["percentage Math"],
-    "percentage in Communication skills": request.form["percentage in CS"],
-    "hours working per day": request.form["hours working per day"],
-    "logical quotient rating": request.form["logical quotient rating"],
-    "hackathons": request.form["hackathon"],
-    "coding skill rating": request.form["coding skill rating"],
-    "public speaking points": request.form["public speaking points"],
-    "can work long time before system?": request.form["can work ltbs"],
-    "self-learning capability?": request.form["self learning capability"],
-    "Extra-courses did": request.form["extra-courses"],
-    "certifications": request.form["certification"],
+    "Acedamic percentage in Operating Systems": request.form["os_marks"],
+    "percentage in Algorithms": request.form["aoa_marks"],
+    "Percentage in Programming Concepts": request.form["percentage PC"], #
+    "Percentage in Software Engineering": request.form["se_marks"],
+    "Percentage in Computer Networks": request.form["cn_marks"],
+    "Percentage in Electronics Subjects": request.form["es_marks"],
+    "Percentage in Computer Architecture": request.form["coa_marks"],
+    "Percentage in Mathematics": request.form["math_marks"],
+    "percentage in Communication skills": request.form["comm_marks"],
+    "hours working per day": request.form["work_hours"],
+    "logical quotient rating": request.form["logic"],
+    "hackathons": request.form["hackathons"],
+    "coding skill rating": request.form["coding skill rating"], #
+    "public speaking points": request.form["communication_skills"],
+    "can work long time before system?": request.form["long_hours"],
+    "self-learning capability?": request.form["self_learner"],
+    "Extra-courses did": request.form["extra_curricular"],
+    "certifications": request.form["certificates"],
     "workshops": request.form["workshops"],
-    "reading and writing skills": request.form["reading and writing"],
-    "memory capability score": request.form["memory capability score"],
-    "Interested subjects": request.form["Interested subjects"],
-    "Interested career area": request.form["Interested career areas"],
-    "Job/Higher Studies?": request.form["job/Higer education"],
-    "Type of company want to settle in?": request.form["type of company"],
-    "Management or techincal": request.form["Management or techincal"],
-    "Salary/work": request.form["Salary/work"],
-    "hard/smart worker": request.form["hard/smart worker"],
-    "worked in teams ever?": request.form["worked in teams"],
-    "Introvert": request.form["Introverted"],
+    "reading and writing skills": request.form["read_write"],
+    "memory capability score": request.form["memory"],
+    "Interested subjects": request.form["interests"],
+    "Interested career area": request.form["career_interests"],
+    "Job/Higher Studies?": request.form["future_plans"],
+    "Type of company want to settle in?": request.form["company_pref"],
+    "Management or techincal": request.form["management_choice"],
+    "Salary/work": request.form["work_salary"],
+    "hard/smart worker": request.form["hard_smart"],
+    "worked in teams ever?": request.form["worked in teams"], #
+    "Introvert": request.form["intovert_not"],
     }
 
     new_res = {}
